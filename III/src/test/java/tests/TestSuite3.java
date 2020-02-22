763cf5fa245f408992d3dba619cbd345b31b6053
suman <suman143learning@gmail.com>
2020-02-22 16:46:49 +0530

    4th

--- III/src/test/java/tests/TestSuite3.java
+++ III/src/test/java/tests/TestSuite3.java
@@ -5,10 +5,10 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestSuite3 {
-
+	// this is sample test
 	@Test
 	public void test() {
-	System.out.println("test123");
+		System.out.println("test12");
 	}
 
 }
